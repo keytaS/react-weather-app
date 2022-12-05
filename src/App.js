7fe51e402ccb5bb85d3a4c7a7d77324d452f0f57
keytaS <wukelanmeinu@gmail.com>
2022-12-05 14:08:07 +0200

    created a basic weather app with current temperature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
-  return <div className="App">weather hi</div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Kyiv" />
 
-export default App;
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://main--jolly-druid-2a995a.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kateryna Saltykova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/keytaS/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://superb-hotteok-302977.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
